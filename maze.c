e1830703ea63439b8d6cd8733ce25afecf131da3
ry <ry.scherbarth@gmail.com>
2023-04-08 20:44:14 -0600

    final

--- maze.c
+++ maze.c
@@ -14,6 +14,7 @@ int size; // number of rows/cols in maze
 void initializeMaze(char maze[][size]);
 void outputMaze(char maze[][size]);
 void getMazeSize();
+bool findRoute(char maze[][size], int row, int col);
 
 int main()
 {
@@ -31,9 +32,13 @@ int main()
   // resulting maze with the route found
   // (or not found, as the case may be)
 
-  outputMaze(maze);
-
-  exit(2); // return the appropirate exit status
+  if(findRoute(maze, 0, 0)){
+    outputMaze(maze);
+    exit(0);
+  }else{
+    outputMaze(maze);
+    exit(2); // return the appropirate exit status
+  }
 }
 
 void initializeMaze(char maze[][size])
@@ -80,4 +85,41 @@ void getMazeSize()
     printf("That is an illegal grid size\n");
     exit(1);
   }
+}
+
+/*
+  Function to try and find any possible way through the maze,
+  tries one position and then recursively tries moving in every
+  other possible location from that spot until it either
+  returns false, no possible route, or true, it found a route.
+*/
+bool findRoute(char maze[][size], int row, int col){
+  // base case
+  if(row < 0 || col < 0 || row >= size || col >= size || maze[row][col] == WALL){
+    return false;
+  }
+
+  // If they start us in our destination
+  if(row == size - 1 && col == size - 1){
+    maze[row][col] = PERSON;
+    return true;
+  }
+
+  // cell has been visited already
+  if(maze[row][col] == BREAD || maze[row][col] == PERSON){
+    return false;
+  }
+
+  // add bread crumb to position we're visitng
+  maze[row][col] = BREAD;
+
+  // re-call the function in each direction until we find a valid one
+  if( findRoute(maze, row-1, col) ||  // north
+      findRoute(maze, row, col+1) ||  // east
+      findRoute(maze, row+1, col) ||  // south
+      findRoute(maze, row, col-1)){   // west
+    maze[row][col] = PERSON;
+    return true;
+  }
+  return false;
 }
